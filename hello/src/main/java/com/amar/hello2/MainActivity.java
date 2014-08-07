2495edb487ca10887b2b5abff7d57dd3af2850f0
sam <amar19860330@gmail.com>
2014-08-07 14:01:10 +0800

    scroll view

--- hello/src/main/java/com/amar/hello2/MainActivity.java
+++ hello/src/main/java/com/amar/hello2/MainActivity.java
@@ -20,6 +20,8 @@ import android.widget.Switch;
 import android.widget.Toast;
 import android.app.ProgressDialog;
 
+import com.amar.hello2.tools.ShortcutUtils;
+
 public class MainActivity extends BaseActivity
 {
     private Button btn1 = null;
@@ -50,107 +52,108 @@ public class MainActivity extends BaseActivity
 
     private OnClickListener onClicklistener = new OnClickListener()
     {
-        public void onClick(View v)
+        public void onClick( View v )
         {
             Intent intent = null;
-            switch (v.getId())
+            switch ( v.getId() )
             {
                 case R.id.button1:
-                    Toast.makeText(MainActivity.this, editText1.getText(), Toast.LENGTH_LONG).show();
+                    Toast.makeText( MainActivity.this, editText1.getText(), Toast.LENGTH_LONG ).show();
                     break;
                 case R.id.button2:
                     intent = new Intent();
-                    intent.setAction(Intent.ACTION_CALL);
-                    Toast.makeText(MainActivity.this, editText2.getText(), Toast.LENGTH_LONG).show();
-                    intent.setData(Uri.parse("tel:" + editText2.getText()));
-                    startActivity(intent);
+                    intent.setAction( Intent.ACTION_CALL );
+                    Toast.makeText( MainActivity.this, editText2.getText(), Toast.LENGTH_LONG ).show();
+                    intent.setData( Uri.parse( "tel:" + editText2.getText() ) );
+                    startActivity( intent );
                     break;
                 case R.id.button3:
                     intent = new Intent();
-                    intent.setClass(MainActivity.this, SecondActivity.class);
-                    intent.putExtra("username", editText1.getText() + "");
-                    intent.putExtra("number", editText2.getText() + "");
+                    intent.setClass( MainActivity.this, SecondActivity.class );
+                    intent.putExtra( "username", editText1.getText() + "" );
+                    intent.putExtra( "number", editText2.getText() + "" );
                     // startActivity( intent );//无返回值
-                    startActivityForResult(intent, REQUEST_CODE);
+                    startActivityForResult( intent, REQUEST_CODE );
                     break;
                 case R.id.button4:
                     intent = new Intent();
-                    intent.setClass(MainActivity.this, ThirdActivity.class);
-                    startActivity(intent);
+                    intent.setClass( MainActivity.this, ThirdActivity.class );
+                    startActivity( intent );
                     break;
                 case R.id.button5:
                     intent = new Intent();
-                    intent.setClass(MainActivity.this, DBActivity.class);
-                    startActivity(intent);
+                    intent.setClass( MainActivity.this, DBActivity.class );
+                    startActivity( intent );
                     break;
                 case R.id.button6:
                     intent = new Intent();
-                    intent.setClass(MainActivity.this, NetWorkActivity.class);
-                    startActivity(intent);
+                    intent.setClass( MainActivity.this, NetWorkActivity.class );
+                    startActivity( intent );
                     break;
                 case R.id.button7:
                     intent = new Intent();
-                    intent.setClass(MainActivity.this, FragmentDemoActivity.class);
-                    startActivity(intent);
+                    intent.setClass( MainActivity.this, FragmentDemoActivity.class );
+                    startActivity( intent );
                     break;
 
                 case R.id.button8:
 
-                    final ProgressDialog pd = ProgressDialog.show(MainActivity.this, "hello", "请等待", true);
+                    final ProgressDialog pd = ProgressDialog.show( MainActivity.this, "hello", "请等待", true );
 
-                    new Thread(new Runnable()
+                    new Thread( new Runnable()
                     {
                         public void run()
                         {
                             try
                             {
-                                Thread.sleep(4000);
+                                Thread.sleep( 4000 );
                                 pd.dismiss();
-                            } catch (InterruptedException e)
+                            }
+                            catch ( InterruptedException e )
                             {
                                 e.printStackTrace();
                             }
                         }
-                    }).start();
+                    } ).start();
                     break;
             }
         }
     };
 
-    public void onclickLayout(View view)
+    public void onclickLayout( View view )
     {
-        Intent intent = new Intent(this, MyLayoutActivity.class);
-        startActivity(intent);
+        Intent intent = new Intent( this, MyLayoutActivity.class );
+        startActivity( intent );
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    protected void onActivityResult( int requestCode, int resultCode, Intent data )
     {
-        if (REQUEST_CODE == requestCode)
+        if ( REQUEST_CODE == requestCode )
         {
-            if (resultCode == SecondActivity.RESULT_CODE)
+            if ( resultCode == SecondActivity.RESULT_CODE )
             {
                 Bundle bundle = data.getExtras();
 
-                String returnValue = bundle.getString(SecondActivity.RESULT_CONTENT);
+                String returnValue = bundle.getString( SecondActivity.RESULT_CONTENT );
 
-                Toast.makeText(MainActivity.this, returnValue, Toast.LENGTH_LONG).show();
+                Toast.makeText( MainActivity.this, returnValue, Toast.LENGTH_LONG ).show();
             }
         }
     }
 
     void drawCircle()
     {
-        RelativeLayout rootLayout = (RelativeLayout) findViewById(R.id.root);
-        final DrawView dv = new DrawView(this);
-        dv.setMinimumHeight(500);
-        dv.setMinimumWidth(300);
-        dv.setOnTouchListener(new View.OnTouchListener()
+        RelativeLayout rootLayout = ( RelativeLayout ) findViewById( R.id.root );
+        final DrawView dv = new DrawView( this );
+        dv.setMinimumHeight( 500 );
+        dv.setMinimumWidth( 300 );
+        dv.setOnTouchListener( new View.OnTouchListener()
         {
             @Override
-            public boolean onTouch(View v, MotionEvent event)
+            public boolean onTouch( View v, MotionEvent event )
             {
-                if (mySwitch.isChecked())
+                if ( mySwitch.isChecked() )
                 {
                     dv.currentX = event.getX();
                     dv.currentY = event.getY();
@@ -162,164 +165,185 @@ public class MainActivity extends BaseActivity
                     return false;
                 }
             }
-        });
-        rootLayout.addView(dv);
+        } );
+        rootLayout.addView( dv );
     }
 
 
     public void initUI()
     {
-        btn1 = (Button) this.findViewById(R.id.button1);
+        btn1 = ( Button ) this.findViewById( R.id.button1 );
 
-        btn2 = (Button) this.findViewById(R.id.button2);
+        btn2 = ( Button ) this.findViewById( R.id.button2 );
 
-        btn3 = (Button) this.findViewById(R.id.button3);
+        btn3 = ( Button ) this.findViewById( R.id.button3 );
 
-        btn4 = (Button) this.findViewById(R.id.button4);
+        btn4 = ( Button ) this.findViewById( R.id.button4 );
 
-        btn5 = (Button) this.findViewById(R.id.button5);
+        btn5 = ( Button ) this.findViewById( R.id.button5 );
 
-        btn6 = (Button) this.findViewById(R.id.button6);
+        btn6 = ( Button ) this.findViewById( R.id.button6 );
 
-        btn7 = (Button) this.findViewById(R.id.button7);
+        btn7 = ( Button ) this.findViewById( R.id.button7 );
 
-        btn8 = (Button) this.findViewById(R.id.button8);
+        btn8 = ( Button ) this.findViewById( R.id.button8 );
 
-        editText1 = (EditText) this.findViewById(R.id.editText1);
+        editText1 = ( EditText ) this.findViewById( R.id.editText1 );
 
-        editText2 = (EditText) this.findViewById(R.id.editText2);
+        editText2 = ( EditText ) this.findViewById( R.id.editText2 );
 
-        mySwitch = (Switch) findViewById(R.id.mySwitch);
+        mySwitch = ( Switch ) findViewById( R.id.mySwitch );
     }
 
     public void uiFunction()
     {
-        btn1.setOnClickListener(onClicklistener);
+        btn1.setOnClickListener( onClicklistener );
 
-        btn2.setOnClickListener(onClicklistener);
+        btn2.setOnClickListener( onClicklistener );
 
-        btn3.setOnClickListener(onClicklistener);
+        btn3.setOnClickListener( onClicklistener );
 
-        btn4.setOnClickListener(onClicklistener);
+        btn4.setOnClickListener( onClicklistener );
 
-        btn5.setOnClickListener(onClicklistener);
+        btn5.setOnClickListener( onClicklistener );
 
-        btn6.setOnClickListener(onClicklistener);
+        btn6.setOnClickListener( onClicklistener );
 
-        btn7.setOnClickListener(onClicklistener);
+        btn7.setOnClickListener( onClicklistener );
 
-        btn8.setOnClickListener(onClicklistener);
+        btn8.setOnClickListener( onClicklistener );
     }
 
 
     int notificationID = 1;
 
-    public void onClickNotification(View view)
+    public void onClickNotification( View view )
     {
-        Intent i = new Intent(this, MainActivity.class);
-        i.putExtra("notification", notificationID);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, i, 0);
+        Intent i = new Intent( this, MainActivity.class );
+        i.putExtra( "notification", notificationID );
+        PendingIntent pendingIntent = PendingIntent.getActivity( this, 0, i, 0 );
 
-        NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        NotificationManager nm = ( NotificationManager ) getSystemService( NOTIFICATION_SERVICE );
 
-        Notification notif = new Notification(R.drawable.ic_launcher, "Reminder: Meeting starts in 5 minutes", System.currentTimeMillis());
+        Notification notif = new Notification( R.drawable.ic_launcher, "Reminder: Meeting starts in 5 minutes", System.currentTimeMillis() );
 
         CharSequence from = "System Alarm";
         CharSequence message = "Meeting with customer at 3pm...";
 
-        notif.setLatestEventInfo(this, from, message, pendingIntent);
+        notif.setLatestEventInfo( this, from, message, pendingIntent );
 
         // ---100ms delay, vibrate for 250ms, pause for 100 ms and
         // then vibrate for 500ms---
-        notif.vibrate = new long[]{100, 250, 100, 500};
-        nm.notify(notificationID, notif);
+        notif.vibrate = new long[]{ 100 , 250 , 100 , 500 };
+        nm.notify( notificationID, notif );
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
+    public boolean onCreateOptionsMenu( Menu menu )
     {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate( R.menu.main, menu );
         return true;
     }
 
     @Override
     protected void onDestroy()
     {
-        Log.i(this.getClass().getName(), "in onDestroy");
+        Log.i( this.getClass().getName(), "in onDestroy" );
         super.onDestroy();
     }
 
     @Override
     protected void onPause()
     {
-        Log.i(this.getClass().getName(), "in onPause");
+        Log.i( this.getClass().getName(), "in onPause" );
         super.onPause();
     }
 
     @Override
     protected void onRestart()
     {
-        Log.i(this.getClass().getName(), "in onRestart");
+        Log.i( this.getClass().getName(), "in onRestart" );
         super.onRestart();
     }
 
     @Override
     protected void onResume()
     {
-        Log.i(this.getClass().getName(), "in onResume");
+        Log.i( this.getClass().getName(), "in onResume" );
         super.onResume();
     }
 
     @Override
     protected void onStart()
     {
-        Log.i(this.getClass().getName(), "in onStart");
+        Log.i( this.getClass().getName(), "in onStart" );
         super.onStart();
     }
 
     @Override
     protected void onStop()
     {
-        Log.i(this.getClass().getName(), "in onStop");
+        Log.i( this.getClass().getName(), "in onStop" );
         super.onStop();
     }
 
     @Override
-    protected void onSaveInstanceState(Bundle outState)
+    protected void onSaveInstanceState( Bundle outState )
     {
-        super.onSaveInstanceState(outState);
-        Log.i(this.getClass().getName(), "in onSaveInstanceState");
+        super.onSaveInstanceState( outState );
+        Log.i( this.getClass().getName(), "in onSaveInstanceState" );
     }
 
     @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState)
+    protected void onRestoreInstanceState( Bundle savedInstanceState )
     {
-        super.onRestoreInstanceState(savedInstanceState);
-        Log.i(this.getClass().getName(), "in onRestoreInstanceState");
+        super.onRestoreInstanceState( savedInstanceState );
+        Log.i( this.getClass().getName(), "in onRestoreInstanceState" );
     }
 
-    public void onclickGotoWidget1(View view)
+    public void onclickGotoWidget1( View view )
     {
-        Intent intent = new Intent(this, Widget1Activity.class);
-        startActivity(intent);
+        Intent intent = new Intent( this, Widget1Activity.class );
+        startActivity( intent );
+    }
+
+    private void addShortcutToDesktop()
+    {
+        Intent shortcut = new Intent( "com.android.launcher.action.INSTALL_SHORTCUT" );
+        // 不允许重建
+        shortcut.putExtra( "duplicate", false );
+        // 设置名字
+        shortcut.putExtra( Intent.EXTRA_SHORTCUT_NAME, this.getString( R.string.app_name ) );
+        // 设置图标
+        shortcut.putExtra( Intent.EXTRA_SHORTCUT_ICON_RESOURCE, Intent.ShortcutIconResource.fromContext( this, R.drawable.ic_launcher ) );
+        // 设置意图和快捷方式关联程序
+        shortcut.putExtra( Intent.EXTRA_SHORTCUT_INTENT, new Intent( this, this.getClass() ).setAction( Intent.ACTION_MAIN ) );
+        // 发送广播
+        sendBroadcast( shortcut );
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
+    protected void onCreate( Bundle savedInstanceState )
     {
-        Log.i(this.getClass().getName(), "in onCreate");
+        Log.i( this.getClass().getName(), "in onCreate" );
 
-        super.onCreate(savedInstanceState);
+        super.onCreate( savedInstanceState );
 
-        setContentView(R.layout.activity_main);
+        setContentView( R.layout.activity_main );
 
         initUI();
 
         uiFunction();
 
         drawCircle();
-    }
 
+        ShortcutUtils shortcutUtils = new ShortcutUtils();
+        String shortcutName = getString( R.string.app_name );
+//        if ( !shortCut.hasShortcut( this, shortcutName ) )
+//        {
+            shortcutUtils.creatShortCut( this, shortcutName, R.drawable.ic_launcher );
+//        }
+    }
 
 }
--- hello/src/main/java/com/amar/hello2/SubPageView.java
+++ hello/src/main/java/com/amar/hello2/SubPageView.java
@@ -1,145 +1,110 @@
 package com.amar.hello2;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
-
-import com.amar.hello2.R;
+import android.widget.EditText;
+import android.widget.ScrollView;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @EActivity(R.layout.activity_sub_page_view)
-public class SubPageView extends Activity
+public class SubPageView extends BaseActivity
 {
-    @ViewById(resName = "subPageBtn1")
-    protected Button subPageBtn1;
+    @ViewById( resName = "scroll_view" )
+    protected ScrollView scrollView;
 
-    @ViewById(resName = "subPageBtn2")
-    protected Button subPageBtn2;
+    @ViewById( resName = "scroll_middle" )
+    protected Button middleBtn;
 
-    @ViewById(resName = "subPageBtn3")
-    protected Button subPageBtn3;
-
-    @ViewById(resName = "subPageBtn4")
-    protected Button subPageBtn4;
-
-    @ViewById(resName = "subPageItem1")
-    protected LinearLayout subPageItem1;
-
-    @ViewById(resName = "subPageItem2")
-    protected LinearLayout subPageItem2;
-
-    @ViewById(resName = "subPageItem3")
-    protected LinearLayout subPageItem3;
-
-    @ViewById(resName = "subPageItem4")
-    protected LinearLayout subPageItem4;
-
-    @ViewById(resName = "subPageContainer")
-    protected RelativeLayout subPageContainer;
-
-    public List<View> pageItemList = new ArrayList<View>();
-
-    public List<Button> pageBtnList = new ArrayList<Button>();
+    @ViewById( resName = "unit_scroll" )
+    protected EditText unitEdtExt;
 
     @AfterViews
     public void afterViews()
     {
-        if( pageItemList.size()==0)
-        {
-            pageItemList.add(subPageItem1);
-            pageItemList.add(subPageItem2);
-            pageItemList.add(subPageItem3);
-            pageItemList.add(subPageItem4);
-
-            pageBtnList.add(subPageBtn1);
-            pageBtnList.add(subPageBtn2);
-            pageBtnList.add(subPageBtn3);
-            pageBtnList.add(subPageBtn4);
-        }
     }
 
-
-    @Click({R.id.subPageBtn1, R.id.subPageBtn2, R.id.subPageBtn3, R.id.subPageBtn4})
-    void clickChangePage(View view)
+    @Click(R.id.up_scroll)
+    void upScroll()
     {
-        Button btn = (Button) view;
-        int index = findIndex(btn);
-        changeView(index);
+        int scrollY = scrollView.getScrollY();
+        scrollHeightOffset( scrollY + getUnit(), scrollView );
     }
 
-    public int findIndex(Button findBtn)
+    @Click( R.id.down_scroll )
+    void downScroll()
     {
-        int index = -1;
-        for (int j = 0; j < pageBtnList.size(); j++)
-        {
-            if (findBtn.getId() == pageBtnList.get(j).getId())
-            {
-                index = j;
-                break;
-            }
-        }
-        return index;
+        int scrollY = scrollView.getScrollY();
+        scrollHeightOffset( scrollY - getUnit(), scrollView );
     }
 
-    public void changeView(int i)
+    @Click( R.id.mid_scroll )
+    void middleScroll()
     {
-        for (int j = 0; j < pageItemList.size(); j++)
+        int[] location = new int[ 2 ];
+        middleBtn.getLocationOnScreen( location );
+        int offset = location[ 1 ] - scrollView.getMeasuredHeight();
+        if ( offset < 0 )
         {
-            View layout = subPageContainer.findViewById(pageItemList.get(j).getId());
-
-            if (i == j)
-            {
-                layout.setVisibility(View.VISIBLE);
-            }
-            else
-            {
-                layout.setVisibility(View.GONE);
-            }
+            offset = 0;
         }
+        scrollHeightOffset( offset, scrollView );
+    }
 
+    void scrollHeightOffset( final int heightOffset, final ScrollView scrollView )
+    {
+        scrollView.post( new Runnable()
+        {
+            @Override
+            public void run()
+            {
+                scrollView.smoothScrollTo( 0, heightOffset );
+            }
+        } );
+    }
+
+    private int getUnit()
+    {
+        String unit = unitEdtExt.getText().toString();
+
+        int unitInt = "".equals( unit ) ? 0 : Integer.parseInt( unit );
+
+        return unitInt;
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState)
+    protected void onCreate( Bundle savedInstanceState )
     {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sub_page_view);
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_sub_page_view );
     }
 
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu)
+    public boolean onCreateOptionsMenu( Menu menu )
     {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.sub_page_view, menu);
+        getMenuInflater().inflate( R.menu.sub_page_view, menu );
         return true;
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item)
+    public boolean onOptionsItemSelected( MenuItem item )
     {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings)
+        if ( id == R.id.action_settings )
         {
             return true;
         }
-        return super.onOptionsItemSelected(item);
+        return super.onOptionsItemSelected( item );
     }
 }
