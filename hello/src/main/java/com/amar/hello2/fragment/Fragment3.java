706821c03cc6b50e46fe9192c52de208f2064676
sam <amar19860330@gmail.com>
2015-02-25 18:04:45 +0800

    scroll listview

--- hello/src/main/java/com/amar/hello2/fragment/Fragment3.java
+++ hello/src/main/java/com/amar/hello2/fragment/Fragment3.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -19,6 +20,7 @@ import java.util.Map;
 
 public class Fragment3 extends Fragment
 {
+    ListView listView;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
@@ -26,15 +28,71 @@ public class Fragment3 extends Fragment
 
         View view = inflater.inflate(R.layout.fragment3, container, false);
         // ---Inflate the layout for this fragment---
-        SimpleAdapter adapter = new SimpleAdapter(getActivity(), getData(), R.layout.vlist,
+        SimpleAdapter adapter = new SimpleAdapter(getActivity(), getData_endness(), R.layout.vlist,
                 new String[]{"vlistTitle", "vlistInfo", "vlistImg"}, new int[]{R.id.vlistTitle, R.id.vlistInfo, R.id.vlistImg});
-        ListView lv = (ListView)( view.findViewById(R.id.fragment3list));
-        lv.setAdapter(adapter);
+        listView= (ListView)( view.findViewById(R.id.fragment3list));
+        listView.setAdapter(adapter);
+        initScrollBtn(view);
 
         return view;
     }
 
-    private List<Map<String, Object>> getData()
+    private void initScrollBtn(View rootView)
+    {
+        Button scrollDownBtn = (Button)rootView.findViewById(R.id.scroll_down);
+        Button scrollUpBtn = (Button)rootView.findViewById(R.id.scroll_up);
+        scrollDownBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                listView.smoothScrollBy(-500,2000);
+            }
+        });
+        scrollUpBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                listView.smoothScrollBy(500,2000);
+            }
+        });
+    }
+
+    private List<Map<String, Object>> getData_endness()
+    {
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+
+        for(int i=0;i<1000;i++)
+        {
+            Map<String, Object> map = new HashMap<String, Object>();
+            if (i % 3 == 0)
+            {
+                map.put("vlistTitle", "G1");
+                map.put("vlistInfo", "google 1");
+                map.put("vlistImg", R.drawable.i1);
+                list.add(map);
+            }
+            if (i % 3 == 1)
+            {
+                map = new HashMap<String, Object>();
+                map.put("vlistTitle", "G2");
+                map.put("vlistInfo", "google 2");
+                map.put("vlistImg", R.drawable.i2);
+                list.add(map);
+            }
+            if (i % 3 == 2)
+            {
+                map.put("vlistTitle", "G3");
+                map.put("vlistInfo", "google 3");
+                map.put("vlistImg", R.drawable.i3);
+                list.add(map);
+            }
+        }
+
+        return list;
+    }
+    private List<Map<String, Object>> getData_old()
     {
         List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 
