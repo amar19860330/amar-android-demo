2563f29298a5fcd4cbaa238b9e2ab7201f25b68e
sam <amar19860330@gmail.com>
2015-04-28 15:18:22 +0800

    加个阴影看看效果

--- hello/src/main/java/com/amar/hello2/fragment/TipDialog.java
+++ hello/src/main/java/com/amar/hello2/fragment/TipDialog.java
@@ -2,7 +2,9 @@ package com.amar.hello2.fragment;
 
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.Context;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -31,10 +33,30 @@ public class TipDialog extends DialogFragment
         Dialog rootDialog = super.onCreateDialog( savedInstanceState );
 
         rootDialog.getWindow().getAttributes().windowAnimations = R.style.my_dialog;
-        WindowManager.LayoutParams lp = rootDialog.getWindow().getAttributes();
-        rootDialog.getWindow().setAttributes( lp );
-        rootDialog.getWindow().setGravity( Gravity.BOTTOM );
+        rootDialog.getWindow().clearFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        rootDialog.getWindow().setGravity( Gravity.CENTER_VERTICAL );
 
         return rootDialog;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        DisplayMetrics dm = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics( dm );
+        int width = dpToPx(300,this.getActivity());
+        getDialog().getWindow().setLayout( width, getDialog().getWindow().getAttributes().height );
+    }
+
+    public int dpToPx(int dp,Context context)
+    {
+        float scale =context.getResources().getDisplayMetrics().density;
+        return (int) (dp * scale + 0.5f);
+    }
+
+    public int pxToDp(float pxValue,Context context)
+    {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
     }
 }
